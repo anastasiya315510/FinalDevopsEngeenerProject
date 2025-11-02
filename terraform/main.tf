4b5e75cf9c70a63a48c0c3a863dfe891f13390b3
Anastasiya <315510a@gmail.com>
2025-11-02 18:17:40 +0200

    try to fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,23 +9,51 @@ terraform {
   required_version = ">= 1.4.0"
 }
 
+# -----------------------------
+# Provider
+# -----------------------------
 provider "aws" {
-  region = var.aws_region
+  region     = var.aws_region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
 
+# -----------------------------
+# Variables
+# -----------------------------
 
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "vpc_id" {}             # VPC ID
+variable "subnet_id" {}          # Subnet ID
+variable "security_group_id" {}  # Security Group ID
+
+# -----------------------------
 # EC2 instance for k3s master node
+# -----------------------------
 resource "aws_instance" "k3s_master" {
   ami           = var.ami_id
   instance_type = var.instance_type
-  subnet_id     = aws_subnet.main.id
+  subnet_id     = var.subnet_id
   key_name      = var.key_name
 
-  vpc_security_group_ids = [aws_security_group.k3s_sg.id]
+  vpc_security_group_ids = [var.security_group_id]
 
+  # user_data.sh should install k3s automatically
   user_data = file("${path.module}/user_data.sh")
 
   tags = {
     Name = "k3s-master"
   }
 }
+
+# -----------------------------
+# Outputs
+# -----------------------------
+
+output "instance_private_ip" {
+  value = aws_instance.k3s_master.private_ip
+}
+# -----------------------------
+# Generate kubeconfig for EKS
+# -----------------------------
