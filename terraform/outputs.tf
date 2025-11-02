ff04fb1acd5cee6075f0842f8c67ec29e5c72fde
Anastasiya <315510a@gmail.com>
2025-11-02 18:17:56 +0200

    try to fix

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,12 +5,4 @@ output "instance_public_ip" {
 output "vpc_id" {
   value = aws_vpc.main.id
 }
-output "cluster_name" {
-  description = "The EKS cluster name"
-  value       = module.eks.cluster_name
-}
 
-output "kubeconfig" {
-  description = "Kubeconfig for the EKS cluster"
-  value       = module.eks.kubeconfig
-}
